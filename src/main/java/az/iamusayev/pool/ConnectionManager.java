6b746f61ee1b93d512137265693a9020621008c9
Anar <95879636+iamusayev@users.noreply.github.com>
2023-07-05 22:07:10 +0400

    Added connection pool (#9)
    Co-authored-by: Anar Musayev <anar.musayev@digirella.az>

---
+++ src/main/java/az/iamusayev/pool/ConnectionManager.java
@@ -0,0 +1,85 @@
+package az.iamusayev.pool;
+
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ConnectionManager {
+
+    private static final String URL_KEY = "db.url";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String PASSWORD_KEY = "db.password";
+    private static final String DRIVER_KEY = "db.driver";
+    private static final String POOL_SIZE_KEY = "db.pool.size";
+    public static final int DEFAULT_POOL_SIZE = 10;
+    private static BlockingQueue<Connection> pool;
+    private static List<Connection> sourceConnections;
+
+    static {
+        loadDriver();
+        initConnectionPool();
+    }
+
+    private static void initConnectionPool() {
+        var poolSize = PropertiesUtil.getProperty(POOL_SIZE_KEY);
+        var size = poolSize == null ? DEFAULT_POOL_SIZE : Integer.parseInt(poolSize);
+        pool = new ArrayBlockingQueue<>(size);
+        sourceConnections = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            var connection = open();
+            var proxyConnection = (Connection) Proxy.newProxyInstance(ConnectionManager.class.getClassLoader(),
+                    new Class[] {ConnectionManager.class},
+                    (proxy, method, args) -> method.getName().equals("close") ? pool.add((Connection) proxy)
+                            : method.invoke(connection, args));
+            pool.add(proxyConnection);
+            sourceConnections.add(connection);
+            pool.add(proxyConnection);
+            sourceConnections.add(connection);
+        }
+    }
+
+    public static void closeConnectionPool() {
+        for (Connection s : sourceConnections) {
+            try {
+                s.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static Connection get() {
+        try {
+            return pool.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private static Connection open() {
+        try {
+            return DriverManager.getConnection(
+                    PropertiesUtil.getProperty(URL_KEY),
+                    PropertiesUtil.getProperty(USERNAME_KEY),
+                    PropertiesUtil.getProperty(PASSWORD_KEY)
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @SneakyThrows
+    private static void loadDriver() {
+        Class.forName(PropertiesUtil.getProperty(DRIVER_KEY));
+    }
+}
